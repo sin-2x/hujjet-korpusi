e59b7a0c1ec42a13f30f697f4e5f45392ddc10db
Abbos <eybbas@gmail.com>
2025-07-01 17:02:01 +0500

    aaaa

--- src/routes/statistics/index.tsx
+++ src/routes/statistics/index.tsx
@@ -4,6 +4,6 @@ import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/statistics/')({
   component: StatisticsPanel,
-  beforeLoad: authGuard
+  //beforeLoad: authGuard
 })
 
